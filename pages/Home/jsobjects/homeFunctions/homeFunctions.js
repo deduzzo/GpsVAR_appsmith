7265ad0884cf1b0c810cbeb79e4ddc6cb0370715
Roberto De Domenico <roberto.dedomenico@asp.messina.it>
2025-04-29 09:38:34 +0000

    costruzione form e logiche

--- pages/Home/jsobjects/homeFunctions/homeFunctions.js
+++ pages/Home/jsobjects/homeFunctions/homeFunctions.js
@@ -1,21 +1,44 @@
 export default {
-		userData: null,
-	distretto: 40,
+	userData: null,
+	secret: "UxZ>69'[Tu<6",
+	distrettiMap: {byUnique: {}, byId: {}},
+	periodo: null,
+	variabiliInserite: [],
 	async initLoad() {
-			this.verifyTokenExpires();
-		},
-		verifyTokenExpires: () => {
+		this.getDistrettiMap();
+		this.verifyTokenExpires();
+		this.getPeriodo();
+		getVariabiliDistretto.run();
+	},
+	getMeseItaliano: (mese) => {
+		let mesi = 'Gennaio_Febbraio_Marzo_Aprile_Maggio_Giugno_Luglio_Agosto_Settembre_Ottobre_Novembre_Dicembre'.split('_');
+		return mesi[mese-1];
+	},
+	getPeriodo: () => {
+		let periodo = getPeriodo.data;
+		this.periodo = periodo.length === 0 ||  periodo.length >1  ? null : periodo[0];
+	},
+	getDistrettiMap: () => {
+		let distretti = getAllDistretti.data;
+		console.log(distretti)
+		for (let distretto of distretti) {
+			this.distrettiMap.byUnique[distretto.unique] = distretto;
+			this.distrettiMap.byId[distretto.old_code] = distretto;
+		}
+	},
+	verifyTokenExpires: () => {
 		let expired = false;
-		console.log(appsmith.store.token);
+		//console.log(appsmith.store.token);
 		if (appsmith.store.token !== null) {
 			try {
 				const decoded = jsonwebtoken.verify(appsmith.store.token, this.secret);
-				//console.log("decoded:");
-				//console.log(decoded);
+				console.log("decoded:");
+				console.log(decoded);
 				this.userData = {
 					username: decoded.data.user, 
 					distretto: decoded.data.id_distretto,
-					mail: decoded.data.mail
+					mail: decoded.data.mail,
+					codDistretto: this.distrettiMap.byUnique[decoded.data.id_distretto].old_code
 				}
 				const newToken = this.createToken({data: decoded.data});
 				//console.log("new token");
@@ -32,7 +55,7 @@ export default {
 		if (expired) {
 			storeValue("token",null);
 			console.log("expired..");
-			navigateTo("Login");
+			navigateTo("LoginPage");
 		}
 		return {expired}
 
@@ -44,19 +67,35 @@ export default {
 		this.scaricaCSV();
 	},
 	generaCSV: () => {
-	let mockData = getAllConvenzionati.data;
+		let mockData = getAllConvenzionati.data;
 		const csv = papaparse.unparse(mockData, {
-		header: true, // questa opzione è di default, ma puoi specificarla
-		delimiter: ";", // puoi cambiare il delimitatore se vuoi
-		newline: "\r\n"
-	});
+			header: true, // questa opzione è di default, ma puoi specificarla
+			delimiter: ";", // puoi cambiare il delimitatore se vuoi
+			newline: "\r\n"
+		});
 
-	// Ora "csv" è una stringa CSV con la prima riga come header
-	return csv;
+		// Ora "csv" è una stringa CSV con la prima riga come header
+		return csv;
 	},
 	scaricaCSV: () => {
-    const csv = this.generaCSV();
-	  console.log(csv);
-    download(csv, "assistiti.csv", "text/csv");
-  }
+		const csv = this.generaCSV();
+		console.log(csv);
+		download(csv, "assistiti.csv", "text/csv");
+	},
+	aggiungiVariabile: () => {
+		if (!variabileSelezionata.selectedOptionValue || !convenzionatoSelezionato.selectedOptionValue || !importoVariabile.text || importoVariabile.text === ""|| parseFloat(importoVariabile.text) === 0.0) 
+			showAlert("Selezionare il convenzionato, il tipo di variabile e l'importo")
+		else {
+			this.variabiliInserite.push({
+				idVariabile: variabileSelezionata.selectedOptionValue,
+				descVariabile: variabileSelezionata.selectedOptionLabel,
+				idConvenzionato: convenzionatoSelezionato.selectedOptionValue,
+				descConvenzionato: convenzionatoSelezionato.selectedOptionLabel,
+				valoreVariabile: parseFloat(importoVariabile.text)
+			})
+			convenzionatoSelezionato.setSelectedOption("");
+			variabileSelezionata.setSelectedOption("");
+			importoVariabile.setValue("");
+		}
+	}
 }
